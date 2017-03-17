f748174e141cb751e0b5848b07851eb016935ccb
hb5021 <noel86@naver.com>
2017-03-17 17:47:00 +0900

    add commit

---
+++ src/main/java/com/project01/controller/ProductsController.java
@@ -0,0 +1,26 @@
+package com.project01.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project01.service.ProductService;
+
+@RestController
+@RequestMapping("/api/main/products/")
+public class ProductsController {
+
+	@Autowired
+	   private ProductService productService;
+	
+	@RequestMapping(value="/{productsCode}", method=RequestMethod.GET)
+	public Map get(@PathVariable("productsCode") String productsCode) {
+		
+		return productService.get(productsCode);
+	}
+	
+}
